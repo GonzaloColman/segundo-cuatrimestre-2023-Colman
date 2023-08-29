ee3f97e8782a75e70f54f6bb24d44f1837cc9403
gonzalo <gonzalogabrielapu@gmail.com>
2023-08-29 19:29:59 -0300

    trabajo

--- kiosko.ts
+++ kiosko.ts
@@ -1,7 +1,39 @@
 /**
  * que vende el kiosco
+ * el stock
  */
 class kiosco {
-
+    private productos: Producto[];
+/**
+ * agrego un producto para vender
+ */
+    agregarProducto(producto:Producto):void{
+        this.productos.push(producto);
+    }
+/**
+ * saco un producto una ves vendido
+ */
+    moverProducto(productoId:number):void{
+        this.productos = this.productos.filter(
+            (producto) => productoId !== productoId
+        )
+    }
 
 }
+/**
+ * una inteface para los productos que venda el kiosco saber cual es y cuantos hay
+ */
+interface Producto{
+    id:number;
+    nombre:string;
+    cantidad:number
+}
+/**
+ * ya lo vendio el kiosco
+ * se deve cargar lo que ya se vendio
+ */
+
+
+/**
+ * 
+ */
